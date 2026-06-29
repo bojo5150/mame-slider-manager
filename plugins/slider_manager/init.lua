4c8f500ebec7aab4d26e7713aedb6a8f7c2c6295
bojo5150 <60300680+bojo5150@users.noreply.github.com>
2026-06-28 20:38:18 -0500

    update

--- plugins/slider_manager/init.lua
+++ plugins/slider_manager/init.lua
@@ -1,7 +1,32 @@
+-----------------------------------------------------------
+--
+-- MAME Slider Manager
+-- Version: 0.1.0-alpha
+--
+-----------------------------------------------------------
+
 local exports = {}
 
+local function log(message)
+    print("[SliderManager] " .. message)
+end
+
 function exports.startplugin()
-    print("MAME Slider Manager v0.1.0 loaded")
+
+    log("Plugin Loaded")
+
+    if manager
+        and manager.machine
+        and manager.machine.system then
+
+        log("Game : " .. manager.machine.system.name)
+
+    else
+
+        log("Unable to determine running game.")
+
+    end
+
 end
 
 return exports
